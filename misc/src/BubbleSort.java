def75979701b85323dfb3d793798567608a73480
Pranav <pranav2579@gmail.com>
2016-05-19 21:17:32 +0530

    hackerrank

---
+++ misc/src/BubbleSort.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+/**
+ * Created by Pranav on 19/05/16.
+ */
+public class BubbleSort {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+        int totalswaps = 0;
+        for (int i = 0; i < n; i++) {
+            int numberOfSwaps = 0;
+
+            for (int j = 0; j < n - 1; j++) {
+                if (a[j] > a[j + 1]) {
+                    int temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    numberOfSwaps++;
+                }
+            }
+
+            totalswaps += numberOfSwaps;
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+
+        System.out.println("Array is sorted in "+ totalswaps +" swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[n-1]);
+
+
+
+    }
+}
